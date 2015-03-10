89088cb088f45a8ac7853e5b01192c1fcf57e5aa
Kirill Timofeev <timofeev.kirill.a@gmail.com>
2015-03-11 02:01:07 +0300

    add error handling

--- cat/cat.c
+++ cat/cat.c
@@ -1,13 +1,21 @@
-#include <helpers.h>
+#include <errno.h>
+#include <string.h>
 #include <stdio.h>
+#include <helpers.h>
 
 #define BUF_SIZE 4096
+
+void print_error() {
+	char* msg = strerror(errno);
+	write_(STDERR_FILENO, msg, strlen(msg) * sizeof(char));
+}
 
 int main() {
 	char buf[BUF_SIZE];
 	while(1) {
 		ssize_t count = read_(STDIN_FILENO, buf, BUF_SIZE);
 		if(count == -1) {
+			print_error();
 			return 1;
 		}
 		if(count == 0) {
@@ -15,8 +23,9 @@ int main() {
 		}
 		ssize_t write_rs = write_(STDOUT_FILENO, buf, count);
 		if(write_rs == -1) {
+			print_error();
 			return 2;
 		}	
 	}	
-return 0;
+	return 0;
 }
