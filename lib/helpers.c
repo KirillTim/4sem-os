66ff004ec4a63aa87197d5f60735de06ee515ea2
Kirill <timofeev.kirill.a@gmail.com>
2015-03-17 11:46:34 +0300

    add read_until to lib

--- lib/helpers.c
+++ lib/helpers.c
@@ -1,5 +1,7 @@
 #include "helpers.h"
 #include <unistd.h>
+#include <stdio.h>
+
 ssize_t read_(int fd, void* buf, size_t count) {
 	size_t offset = 0;
 	for(;;) {
@@ -25,5 +27,31 @@ ssize_t write_(int fd, const void* buf, size_t count) {
 		if(offset >= count) {
 			return offset;
 		}
+	}
+}
+
+ssize_t read_until(int fd, void * buf, size_t count, char delimiter) {
+	ssize_t offset = 0;
+	for(;;) {
+		ssize_t r = read(fd, buf + offset, count - offset);
+		fprintf( stderr, "offset: %zu \n" , offset);
+		fprintf( stderr, "r: %zu \n" , r);
+		for (int i = 0; i < r; i++) {
+			if (((char*)buf)[i + offset] == delimiter) {
+				fprintf( stderr, "i: %zu \n" , i);
+				return offset + i;
+			}
+		}
+		if (r == count) {
+			return count;
+		}
+		if (r == -1) {
+			return -1;
+		}
+		if (r == 0) {
+			return offset;
+		}
+
+		offset += r;	
 	}
 }
